2c3227de443bcd21e3f5725e394ffdf7fdb952e9
Aboo Thahir <thahulive@gmail.com>
2019-01-12 21:25:24 +0530

    dynamic component with example

---
+++ dynamic-component/vue-js-dynamic-component.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <component 
+      :is="template" />
+  </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            isAdminUser: true
+        }
+    },
+    computed: {
+        template() {
+            let template = (this.isAdminUser) ? 'admin-dashboard' : 'user-dashboard'
+            return () => import(`example-components/${template}.vue`)
+        }
+    }
+}
+</script>
